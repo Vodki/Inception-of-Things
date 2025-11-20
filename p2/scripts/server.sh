5d723dfc99e07c336e4f9b32f12ce0d12a46da99
Vodki <sylvestre.louis26@outlook.fr>
2025-11-20 17:44:33 +0100

    P2 done (To give a better check)

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -1,4 +1,8 @@
-sudo dnf update -y
+echo ">>> [Fix] Setting DNS to 8.8.8.8..."
+echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
+
+sudo apt-get update -y
+sudo apt-get install -y curl
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -
 
