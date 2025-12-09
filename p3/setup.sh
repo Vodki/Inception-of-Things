c409084292e91d72dba849a4c80649b65bcb9449
Vodki <sylvestre.louis26@outlook.fr>
2025-12-09 20:33:43 +0100

    port mapping in setup.sh

--- p3/setup.sh
+++ p3/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-k3d cluster create IoT 
+k3d cluster create IoT -p "8080:80@loadbalancer" -p "8443:443@loadbalancer"
 kubectl create namespace argocd
 kubectl create namespace dev
 kubens argocd
